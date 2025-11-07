5c955805ef982c3c13bd0d3eb0f83600b401c221
viylouu <131220600+viylouu@users.noreply.github.com>
2025-11-07 15:49:29 -0700

    pizza

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@ class Game : public tail::Program {
 public:
     void init(tail::Settings* sets) {
         sets->bgcolor = v3{.2,.4,.3};
+
+        tail::Node* n = scene->add_child(new tail::Node());
+        tail::Renderer2d* c = (tail::Renderer2d*)n->add_component(new tail::Renderer2d());
+        c->typedata = tail::Renderer2d::Rect{ .col = v4{1,0,0,1} };
+
+        n->scale = v3{64,64,1};
     }
 };
 
