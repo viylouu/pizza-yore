f97e313f19c4354725497263b2edabc15699f323
viylouu <131220600+viylouu@users.noreply.github.com>
2025-11-07 16:25:56 -0700

    aah yes

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,17 @@ public:
     void init(tail::Settings* sets) {
         sets->bgcolor = v3{.2,.4,.3};
 
+        v4 sample{0,0,16,32};
+
         tail::Node* n = scene->add_child(new tail::Node());
         tail::Renderer2d* c = (tail::Renderer2d*)n->add_component(new tail::Renderer2d());
-        c->typedata = tail::Renderer2d::Rect{ .col = v4{1,0,0,1} };
+        c->typedata = tail::Renderer2d::Tex{ 
+            .tex = new tail::Texture("data/sprites/player.png"), 
+            .tint = v4{1},
+            .sample = sample
+        };
 
-        n->scale = v3{64,64,1};
+        n->scale = v3{sample.z,sample.w,1};
     }
 };
 
