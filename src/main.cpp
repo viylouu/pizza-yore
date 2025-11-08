e98b3543c69b42865fb72aa62e0c885faa890bbb
viylouu <131220600+viylouu@users.noreply.github.com>
2025-11-08 11:08:26 -0700

    name

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,24 @@ public:
     void init(tail::Settings* sets) {
         sets->bgcolor = v3{.2,.4,.3};
 
-        tail::Node* camn = scene->add_child(new tail::Node());
-        tail::Camera* cam = (tail::Camera*)camn->add_component(new tail::Camera(480,270));
-        cam->is_master = true;
+        tail::Node* cam = scene->add_child(new tail::Node());
+        cam->name = "camera";
+        tail::Camera* cam_cam = (tail::Camera*)cam->add_component(new tail::Camera(480,270));
+        cam_cam->is_master = true;
 
         v4 sample{0,0,16,32};
 
-        tail::Node* n = scene->add_child(new tail::Node());
-        tail::Renderer2d* c = (tail::Renderer2d*)n->add_component(new tail::Renderer2d());
-        c->typedata = tail::Renderer2d::Tex{ 
+        tail::Node* pbod = scene->add_child(new tail::Node());
+        pbod->name = "player body";
+        tail::Renderer2d* pbod_r2d = (tail::Renderer2d*)pbod->add_component(new tail::Renderer2d());
+        pbod_r2d->typedata = tail::Renderer2d::Tex{ 
             .tex = new tail::Texture("data/sprites/player.png"), 
             .tint = v4{1},
             .sample = sample
         };
-        c->cams.push_back(cam);
+        pbod_r2d->cams.push_back(cam_cam);
 
-        n->scale = v3{sample.z,sample.w,1};
+        pbod->scale = v3{sample.z,sample.w,1};
     }
 };
 
